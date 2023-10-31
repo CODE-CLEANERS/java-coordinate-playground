74250d600bab8a98a72f09192ce85d8de40e1457
DK <laancer4@gmail.com>
2023-10-31 10:01:56 +0900

    feat : Custom 인터페이스를 이용한 Function 활용 (팩토리)

--- src/main/java/figure/FigureFactoryLambda.java
+++ src/main/java/figure/FigureFactoryLambda.java
@@ -10,6 +10,12 @@ public class FigureFactoryLambda {
         throw new IllegalArgumentException("util class");
     }
     private static final Map<Integer, Function<List<Dot>, Figure>> figureMap = new HashMap<>();
+    private static final Map<Integer, FactoryFunction> figureMap2 = new HashMap<>();
+    static {
+        figureMap2.put(2, Line::new);
+        figureMap2.put(3, Triangle::new);
+        figureMap2.put(4, Square::new);
+    }
     static {
         figureMap.put(2, Line::new);
         figureMap.put(3, Triangle::new);
@@ -18,5 +24,9 @@ public class FigureFactoryLambda {
     public static Figure getInstance(List<Dot> dotList){
         Function<List<Dot>, Figure> figureFunction = figureMap.get(dotList.size());
         return figureFunction.apply(dotList);
+    }
+    public static Figure getInstanceByCustomFunction(List<Dot> dotList){
+        FactoryFunction factoryFunction = figureMap2.get(dotList.size());
+        return factoryFunction.create(dotList);
     }
 }
